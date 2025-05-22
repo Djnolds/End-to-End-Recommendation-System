c27997a3700779eb7955d0b84653210a4f4acb6a
Djnolds <ayoreynolds@gmail..com>
2025-05-22 11:54:35 +0100

    file updated

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
 
-project_name = "books_recommendetion"
+project_name = "books_recommendation"
 
 
 list_of_files = [
